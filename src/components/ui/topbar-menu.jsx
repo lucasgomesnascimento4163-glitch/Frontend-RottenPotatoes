939daa21f00da086a334177f4b483c2e822b0d24
lucasgomesnascimento4163 <lucasgomesnascimento4163@gmail.com>
2026-04-17 18:54:53 -0400

    alterações

--- src/components/ui/topbar-menu.jsx
+++ src/components/ui/topbar-menu.jsx
@@ -5,7 +5,7 @@ import { Separator } from "radix-ui";
 export function TopbarMenu() {
     return (
         <header className="flex items-center gap-9 h-20">
-            <Link href="/home"><h1 className="text-5xl font-extrabold whitespace-nowrap">🥔 Rotten<span className="text-amber-700">Potatoes</span></h1></Link>
+            <Link href="/"><h1 className="text-5xl font-extrabold whitespace-nowrap">🥔 Rotten<span className="text-amber-700">Potatoes</span></h1></Link>
             <nav className="flex justify-between items-center w-full">
              <div className="flex gap-6 text-sm">
                 <Link href="/best">Melhores</Link>
