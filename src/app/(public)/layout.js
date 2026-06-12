a1d50f74e7052c3d793b24a98ce128f7fc0378c3
lucasgomesnascimento4163 <lucasgomesnascimento4163@gmail.com>
2026-06-11 21:23:14 -0400

    mudança

--- src/app/(public)/layout.js
+++ src/app/(public)/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { cn } from "@/lib/utils";
 import { TopbarMenu } from "@/components/ui/topbar-menu";
 
